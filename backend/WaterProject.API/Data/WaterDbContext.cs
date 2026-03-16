cff64ac94302b00a232de8e1ad6c0a920beade74
Hector Casablanca <jeremiahedu2019@gmail.com>
2026-03-15 21:43:14 -0600

    Initial Commit

---
+++ backend/WaterProject.API/Data/WaterDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace WaterProject.API.Data
+{
+    public class WaterDbContext : DbContext
+    { 
+
+     public WaterDbContext(DbContextOptions<WaterDbContext> options): base(options)
+        { 
+            
+        }
+        public DbSet <Project> Projects {  get; set; }
+    }
+}
